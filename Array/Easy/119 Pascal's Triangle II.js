4bd9a1d8fb7e63847f53c63ceb0012dda696071b
Steven <stevenlin3263@gmail.com>
2023-03-08 06:45:38 +0800

    119 Pascal's Triangle II method 2

--- Array/Easy/119 Pascal's Triangle II.js
+++ Array/Easy/119 Pascal's Triangle II.js
@@ -30,3 +30,18 @@ var getRow = function(rowIndex) {
       index++
   }
 };
+
+/** Method 2 advanced
+ * Strategy
+ * 
+ * 
+*/
+
+var getRow = function(rowIndex) {
+  let res = Array(rowIndex+1)
+  res[0] = 1
+  for (let i = 1; i < rowIndex + 1; i++) {
+    res[i] = res[i-1] * ((rowIndex - i + 1)/i)
+  }
+  return res
+};
