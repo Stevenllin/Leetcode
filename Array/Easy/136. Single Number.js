d6b5917fd8497c6ab25ddf39eec3886c200a16d3
Steven <stevenlin3263@gmail.com>
2023-03-09 09:15:52 +0800

    136. Single Number method 2

--- Array/Easy/136. Single Number.js
+++ Array/Easy/136. Single Number.js
@@ -4,8 +4,9 @@
  * @obj is an object to store all element as key and the number of occurrences of the element as value
  * 
  * And use the for loop to check @obj and return the key which the value is equal to 1
+ * 
+ * Time complexity O(n)
 */
-
 var singleNumber = function(nums) {
   let obj = {}
 
@@ -22,4 +23,29 @@ var singleNumber = function(nums) {
       return item
     }
   }
+};
+
+/** method 2
+ * Strategy 
+ * 
+ * only compare the first and the second element @nums if it's not equal then remove these two elements from @nums
+ * otherwise break the for loop and return the first element
+ * 
+ * Notice that we need to store @length first
+ * because the length of @nums will be changed after removing element
+ * 
+ * this method is slower than the method 1 because of sort method
+*/
+var singleNumber = function(nums) {
+  const length = nums.length
+  nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < length; i++) {
+    if (nums[0] === nums[1]) {
+      nums.splice(0, 2)
+    } else {
+        break
+    }
+  }
+  return nums[0]
 };
