4f802182925a514283e1ca716c49d2d3fe6c8248
Steven <stevenlin3263@gmail.com>
2023-03-10 09:01:47 +0800

    169. Majority Element method 2

--- Array/Easy/169. Majority Element.js
+++ Array/Easy/169. Majority Element.js
@@ -22,3 +22,24 @@ var majorityElement = function(nums) {
     }
   }
 };
+
+/** method 1
+ * the only difference between method 1 and method 2 is using the Map data structure
+*/
+
+var majorityElement = function(nums) {
+  let map = new Map()
+
+  for(let i = 0; i < nums.length; i++){
+      let targetTimes = map.get(nums[i])
+
+      if(targetTimes == undefined){
+          map.set(nums[i], 1)
+      }else{
+          map.set(nums[i], targetTimes+1)
+          if (targetTimes+1) {
+            return nums[i]
+          }
+      }
+  }
+};
