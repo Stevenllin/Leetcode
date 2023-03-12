bfe10baf0aa7fd5ed152d3999685493c46581964
Steven <stevenlin3263@gmail.com>
2023-03-12 12:52:35 +0800

    268. Missing Number method 2

--- Array/Easy/268. Missing Number.js
+++ Array/Easy/268. Missing Number.js
@@ -4,6 +4,8 @@
  * @sum the sum of each number in array @nums 
  * 
  * this method is to sum up [0, nums.length] then minus @sum
+ * 
+ * Time complexity is O(1)
 */
 
 var missingNumber = function(nums) {
@@ -12,4 +14,22 @@ var missingNumber = function(nums) {
     sum = sum + nums[i]
   }
   return ((1 + nums.length)/2) * nums.length - sum
+};
+
+/** method 2
+ * Strategy
+ * 
+ * Array method includes or indexOf time complexity is O(n) 
+ * 
+ * So the time complexity is O(n^2)
+*/
+var missingNumber = function(nums) {
+  let index = 0
+  for(let i = 0; i < nums.length; i++){
+    if (!nums.includes(index)) {
+      return index
+    }
+    index++
+  }
+  return nums.length
 };
