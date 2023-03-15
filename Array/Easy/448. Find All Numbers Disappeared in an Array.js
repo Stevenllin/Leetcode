509a3c922cf8b55034f5b349304061d11b9eba47
Steven <stevenlin3263@gmail.com>
2023-03-15 11:46:57 +0800

    448. Find All Numbers Disappeared in an Array method 2

--- Array/Easy/448. Find All Numbers Disappeared in an Array.js
+++ Array/Easy/448. Find All Numbers Disappeared in an Array.js
@@ -24,3 +24,20 @@ var findDisappearedNumbers = function(nums) {
 
   return result
 };
+
+/** method 2 
+ * Strategy
+ * 
+ * Time complexity: O(n^2)
+*/
+
+var findDisappearedNumbers = function(nums) {
+  let result = []
+
+  for(let i = 0; i < nums.length; i++){
+    if(!nums.includes(i+1)){
+      result.push(i+1)
+    }
+  }
+  return result
+};
