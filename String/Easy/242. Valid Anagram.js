2d95f363d5ae557775c5301002ec2ac0483cbed4
Steven <stevenlin3263@gmail.com>
2023-03-24 10:17:54 +0800

    242. Valid Anagram method2

--- String/Easy/242. Valid Anagram.js
+++ String/Easy/242. Valid Anagram.js
@@ -1,6 +1,7 @@
 /** method 1
  * 
- * Time Complexity O(n)
+ * 
+ * Time Complexity O(3n)
  * However, this method use 2 times sort(). So it is not really good method
  */
 var isAnagram = function(s, t) {
@@ -11,6 +12,36 @@ var isAnagram = function(s, t) {
 
   for(let i = 0; i < arrayT.length; i++){
     if(arrayT[i] !== arrayS[i]) return false
+  }
+  return true
+};
+
+/** method 2
+ * 
+ * Store all char and those appereace times in @objS
+ * 
+ * And use for loop to check if all char from @t is exist in @objS
+ * 
+ * Time Complexity O(2n)
+*/
+
+var isAnagram = function(s, t) {
+  if(s.length !== t.length) return false
+  
+  const objS = {}
+  for(let i = 0; i < s.length; i++){
+    if(objS[s[i]]){
+      objS[s[i]] = objS[s[i]] + 1
+    }else{
+      objS[s[i]] = 1
+    }
+  }
+  for(let i = 0; i < t.length; i++){
+    if(!objS[t[i]]){
+      return false
+    }else{
+      objS[t[i]] = objS[t[i]] - 1
+    }
   }
   return true
 };
