42d9c814ef25b2e2e5276ea8cbcd86f4b96b1131
Steven <stevenlin3263@gmail.com>
2023-03-25 21:40:51 +0800

    387. First Unique Character in a String method 2

--- String/Easy/387. First Unique Character in a String.js
+++ String/Easy/387. First Unique Character in a String.js
@@ -24,3 +24,23 @@ var firstUniqChar = function(s) {
   }
   return -1
 };
+
+/** method 2
+ * 
+ * create new array @targetArray in each round and remove the current round of char from @targetArray
+ * use indexOf method to find out the rest of char in @targetArray
+ * 
+ * 
+ * Time Limit Exceeded Not suggest
+ */
+
+ var firstUniqChar = function(s) {
+  for(let i = 0; i < s.length; i++){
+    const targetArray = [...s]
+    targetArray.splice(i, 1)
+    if(targetArray.indexOf(s[i]) === -1){
+      return i
+    }
+  }
+  return -1
+};
