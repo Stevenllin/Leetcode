2c65b0169123df516422578cbf7a1b5609bd1813
Steven <stevenlin3263@gmail.com>
2023-03-31 09:10:38 +0800

    83. Remove Duplicates from Sorted List

---
+++ Linked List/Easy/83. Remove Duplicates from Sorted List.js
@@ -0,0 +1,18 @@
+/** method 1 
+ * 
+ */
+ var deleteDuplicates = function(head) {
+  if(head === null || head.next === null) return head;
+
+  let current = head
+  
+  while (current.next !== null) {
+    if (current.val !== current.next.val) {
+      current = current.next
+    }else{
+      current.next = current.next.next
+    }
+  }
+  return head
+};
+
