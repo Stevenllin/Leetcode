b676e0f2492900824ffae468422201c96aac2cef
Steven <stevenlin3263@gmail.com>
2023-04-19 08:42:04 +0800

    55. Jump Game

---
+++ Array/Medium/55. Jump Game.js
@@ -0,0 +1,12 @@
+/** method 1 
+ * 
+ */
+var canJump = function(nums) {
+  let max = 0
+
+  for(let i = 0; i < nums.length; i++){
+    if(i > max) return false
+    max = Math.max(max, i + nums[i])
+  }
+  return true
+};
