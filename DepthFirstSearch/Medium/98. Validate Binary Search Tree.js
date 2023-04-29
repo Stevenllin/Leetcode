800486ce5dcd130f0234336f612ad2b7fe57d0a6
Steven <stevenlin3263@gmail.com>
2023-04-29 15:46:14 +0800

    98. Validate Binary Search Tree

--- DepthFirstSearch/Medium/98. Validate Binary Search Tree.js
+++ DepthFirstSearch/Medium/98. Validate Binary Search Tree.js
@@ -2,16 +2,13 @@
  * 
  */
 var isValidBST = function(root) {
-  return helper(root, null, null)
+  return helper(root, null, null);
+}
 
-  function helper (node, low, high) {
-    if (node === null) return true
-    const value = node.val
-
-    if ((low && low >= value) || (high && high <= value)) {
-      return false
-    }
-
-    return helper(node.right, value, high) && helper(node.left, low, value)
-  }
+function helper(node, low, high) {
+  if (node === null) return true;
+  const val = node.val;
+  if ((low !== null && val <= low) || (high !== null && val >= high)) 
+      return false;
+  return helper(node.right, val, high) && helper(node.left, low, val);
 }
