97bd2c1e022adecaf8af3144d6ebebf6c6a01367
Steven <stevenlin3263@gmail.com>
2023-05-10 11:59:50 +0800

    189. Rotate Array

--- Array/Medium/189. Rotate Array.js
+++ Array/Medium/189. Rotate Array.js
@@ -1,4 +1,4 @@
-/** method 1 Time Limit Exceeded
+/** method 1 
  * 
  */
 var rotate = function(nums, k) {
@@ -6,5 +6,18 @@ var rotate = function(nums, k) {
   for (let i = 0 ; i < k ; i++){
     let value = nums.pop();
     nums.unshift(value);
+  }
+};
+
+/** method 2
+ * 
+ */
+var rotate = function (nums, k) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    nums[i + k] = nums[i];
+  }
+
+  for (let j = k - 1; j >= 0; j--) {
+    nums[j] = nums.pop();
   }
 };
