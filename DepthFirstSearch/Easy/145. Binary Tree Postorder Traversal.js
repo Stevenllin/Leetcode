93e2a1a15c0c9c053df812cdbac68d7f538e6472
Steven <stevenlin3263@gmail.com>
2023-06-08 16:32:48 +0800

    145. Binary Tree Postorder Traversal

--- DepthFirstSearch/Easy/145. Binary Tree Postorder Traversal.js
+++ DepthFirstSearch/Easy/145. Binary Tree Postorder Traversal.js
@@ -1,5 +1,10 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 利用閉包的概念，節點先往左移、再右移，儲存當前節點 @root val 於 @array
  * 
+ * 時間複雜度為 O(n)
  */
 var postorderTraversal = function(root) {
   if (!root) return []
