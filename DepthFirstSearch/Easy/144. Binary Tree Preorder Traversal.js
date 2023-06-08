3423b5f1c7c925cf967b17304f09c2545ae1d5e6
Steven <stevenlin3263@gmail.com>
2023-06-08 16:22:39 +0800

    144. Binary Tree Preorder Traversal

--- DepthFirstSearch/Easy/144. Binary Tree Preorder Traversal.js
+++ DepthFirstSearch/Easy/144. Binary Tree Preorder Traversal.js
@@ -1,10 +1,15 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 利用閉包的概念，先儲存當前節點 @root val 於 @array ，節點往左移、再右移
  * 
+ * 時間複雜度為 O(n)
 */
 var preorderTraversal = function(root) {
   if(!root) return []
-  const array = []
 
+  const array = []
   addElement(root)
 
   function addElement(root){
