a530a768299f78e36640c06d8a342db0017aac21
Steven <stevenlin3263@gmail.com>
2023-06-09 09:17:34 +0800

    101. Symmetric Tree

--- BreadthFirstSearch/Easy/101. Symmetric Tree.js
+++ BreadthFirstSearch/Easy/101. Symmetric Tree.js
@@ -1,11 +1,15 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：概念與第 100 題類似
  * 
+ * 時間複雜度為 O(n)
  */
 var isSymmetric = function(root) {
   return checkIsSymmetric(root, root)
+
   function checkIsSymmetric(p, q){
     if (p === null && q === null) return true
-
     if (p === null && q !== null || p !== null && q === null) return false
   
     if (p.val !== q.val) return false
