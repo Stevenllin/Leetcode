11a27b98c207e90a512f4ff27bf49e9729c822d8
Steven <stevenlin3263@gmail.com>
2023-06-11 20:24:02 +0800

    83. Remove Duplicates from Sorted List

--- Linked List/Easy/83. Remove Duplicates from Sorted List.js
+++ Linked List/Easy/83. Remove Duplicates from Sorted List.js
@@ -1,16 +1,20 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：比較當前節點與下個節點的 val 是否相等。若相等，將當前節點的 next 連接至下下個節點
  * 
+ * 時間複雜度為 O(n)
  */
-var deleteDuplicates = function(head) {
-  if(head === null || head.next === null) return head;
+ var deleteDuplicates = function(head) {
+  if (!head || !head.next) return head
 
   let current = head
-  
-  while (current.next !== null) {
-    if (current.val !== current.next.val) {
-      current = current.next
-    }else{
-      current.next = current.next.next
+
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+        current.next = current.next.next
+    } else {
+        current = current.next
     }
   }
   return head
