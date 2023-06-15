b76ba4313c8599b08bf86f7e10dda9096c2852ad
Steven <stevenlin3263@gmail.com>
2023-06-15 09:35:30 +0800

    141. Linked List Cycle

--- Linked List/Easy/141. Linked List Cycle.js
+++ Linked List/Easy/141. Linked List Cycle.js
@@ -1,19 +1,21 @@
-/** method 1 
- * 
- * if @flag is true it means this node is looped before
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：凡經過得節點皆設定一個 flag 為 true 用於紀錄此節點已走過
  * 
  * Time complexity O(n)
 */
 
 var hasCycle = function(head) {
-  if (head === null || head.next === null) return null
+  if (!head || !head.next) return false
 
   while (head.next) {
-    if(head.flag){
+    if (head.flag) {
       return true
     }
     head.flag = true
     head = head.next
   }
+
   return false
 };
