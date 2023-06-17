01d367439eea6b1ed9cfa49573ebccdfd6b7cf4e
Steven <stevenlin3263@gmail.com>
2023-06-17 09:08:25 +0800

    35 SearchInsertPosition

--- Array/Easy/35 SearchInsertPosition.js
+++ Array/Easy/35 SearchInsertPosition.js
@@ -1,71 +1,31 @@
-/** Method 1 Binary Search
- * Strategy
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：從題目中提到規定時間複雜度必須為 O(logn)，以及提示 @nums 為已排序好的陣列，因此，可以透過 Binary Search 實現作法
  * 
- * if target is exist in array @nums then array method indexOf can find out the result
- * otherwise usind binary search method and return rightIndex + 1
- * 
- * Notice that while loop condition should contains equal operator to find out when target is smaller than the smallest number in @nums
- * the time complexity could be O(logn)
+ * 時間複雜度為 O(logn)
 */
 var searchInsert = function(nums, target) {
-  if (nums.indexOf(target) >= 0) return nums.indexOf(target)
+  let left = 0
+  let right = nums.length - 1
 
-  let leftIndex = 0;
-  let rightIndex = nums.length - 1;
-
-  while (leftIndex <= rightIndex) {
-    const middleIndex = Math.floor((leftIndex+rightIndex)/2)
-    if (nums[middleIndex] > target) {
-      rightIndex = middleIndex - 1
-    } else if (nums[middleIndex] < target) {
-      leftIndex = middleIndex + 1
-    } else {
-      return middleIndex;
+  while(left <= right){
+    let middle = Math.floor((left+right)/2)
+    if(nums[middle] > target){
+      right = middle - 1
+    }else if(nums[middle] < target){
+      left = middle + 1
+    }else{
+      return middle
     }
   }
+  return left
+}
 
-  return rightIndex + 1
-};
-
-/** Method 2 
- * Strategy
- * This method is add target to nums and sort again
+/** Method 2
+ * 策略：若不考慮時間複雜度的規定，可以透過 filter 小於目標值，快速找到答案。若未能找到，即返回陣列長度大小，代表目標值大於陣列內的所有值
  * 
- * the time complexity could be O(n)
-*/
-var searchInsert = function(nums, target) {
-  nums.push(target)
-  nums.sort((a, b) => a - b)
-  
-  return nums.indexOf(target)
-};
-
-/** Method 3
- * Stragety
- * @currentIndex is the index we want to find out
- * 
- * the method is to find the first number in @nums is bigger than target and return that number index @currentIndex
- * if there is no number bigger than target then return 0 (default number is 0)
-*/
-var searchInsert = function(nums, target) {
-  if (nums.indexOf(target) >= 0) return nums.indexOf(target)
-
-  let currentIndex = 0;
-  let currentValue = nums[currentIndex]
-
-  while(currentValue < target) {
-    currentIndex++
-    currentValue = nums[currentIndex]
-  }
-  return currentIndex
-};
-
-/** Method 4
- * Strategy
- * @targetNumber is the first number bigger than or equal to the @target
- * 
- * the index of @targetNumber is what we want
- * And if there is all number in @nums is smaller than target then return the length of @nums
+ * 時間複雜度為 最小 O(1) 最大 O(n)
 */
 var searchInsert = function(nums, target) {
   const targetNumber = nums.filter(item => item >= target)[0]
