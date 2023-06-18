ee48542bb92889a6909f469b4d984a69ffbc107f
Steven <stevenlin3263@gmail.com>
2023-06-19 06:35:44 +0800

    58. Length of Last Word

--- String/Easy/58. Length of Last Word.js
+++ String/Easy/58. Length of Last Word.js
@@ -1,8 +1,9 @@
-/** method 1
- * 
- * convert string @s to array @array to remove all the space
+/** 練習兩次 */
+
+/** Method 1
+ * 策略：以空格為 split 的目標，並將有有空格的 filter，最後，再將使用 pop 取得最後一個字串
  *  
- * Time complexity O(n)
+ * 時間複雜度為 O(n)
  */
 var lengthOfLastWord = function(s) {
   const array = s.split(' ').filter(item => item.length !== 0)
