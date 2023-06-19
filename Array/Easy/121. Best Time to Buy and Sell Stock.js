ae1913ea6f7b3ff05d9773c26f74f1a7cb0135bf
Steven <stevenlin3263@gmail.com>
2023-06-19 09:06:32 +0800

    121. Best Time to Buy and Sell Stock

--- Array/Easy/121. Best Time to Buy and Sell Stock.js
+++ Array/Easy/121. Best Time to Buy and Sell Stock.js
@@ -1,43 +1,20 @@
-/** method 1 Brute Force
+/** 練習兩次 */
+
+/**  Method 1
+ * 策略：
+ * 設定一個紀錄當前最小值 @min 與紀錄當前差價的最大值 @maxProfit
+ * 若 當前值 - @min 是大於 @maxProfit ，設定此差值為 @maxProfit
+ * 若 當前值小於 @min ，設定當前值為 @min
  * 
- * THIS method will result Time Limit Exceeded (Not suggest)
- * 
- * Time complexity O(n*n)
+ * 時間複雜度為 O(n)
 */
 var maxProfit = function(prices) {
   let maxProfit = 0
+  let min = prices[0]
 
-  for (let i = 0; i < prices.length - 1; i++) {
-      for (let j = i+1; j < prices.length; j++) {
-          if (prices[j] - prices[i] > maxProfit) {
-              maxProfit = prices[j] - prices[i]
-          }
-      }
-  }
-
-  return maxProfit
-};
-
-/**  method 2 advanced
- * @min is the smallest number until current loop
- * @maxProfit is the maximum of profit until current loop
- * 
- * The method is to find @min and calculate the @maxProfit at the same time
- * 
- * Time complexity O(n)
-*/
-var maxProfit = function(prices) {
-  let min = Number.MAX_SAFE_INTEGER;
-  let maxProfit = 0;
-
-  for (let i = 0; i < prices.length; i++) {
-    if (prices[i] < min) {
-      min = prices[i]
-    }
-
-    if (prices[i] - min > maxProfit) {
-      maxProfit = prices[i] - min
-    }
+  for(let i = 0; i < prices.length; i++){
+    if(prices[i] < min) min = prices[i]
+    if(prices[i] - min > maxProfit) maxProfit = prices[i] - min
   }
 
   return maxProfit
