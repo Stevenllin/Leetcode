7d839a31348f96ccfba33f522b23a90bb7735c93
Steven <stevenlin3263@gmail.com>
2023-06-20 06:38:52 +0800

    13. Roman to Integer

--- String/Easy/13. Roman to Integer.js
+++ String/Easy/13. Roman to Integer.js
@@ -1,31 +1,33 @@
-/** method 1 
- * when the next number @num2 is bigger than @num1 then add @num2 - @num1 to result
- * 
- * 
- * Notice that increase the index @i instead of removing element from array @s
- * Time complexity O(n)
- */
+/** 練習兩次 */
 
+/** Method 1 
+ * 策略：
+ * 透過範例可觀察到，兩種邏輯
+ * 1. 當前字母對應的數字大於下一個字母對應的數字，將當前字母對應的數字加入 @result ；
+ * 2. 當前字母對應的數字小於下一個字母對應的數字，將下一個字母對應的數字 - 當前字母對應的數字 加入 @result
+ * 
+ * 注意：若為第二個邏輯時，需跳過下一輪遞迴，將 @i + 1
+ * 
+ * 時間複雜度為 O(n)
+ */
 var romanToInt = function(s) {
   let map = {
-      I: 1,
-      V: 5,
-      X: 10,
-      L: 50,
-      C: 100,
-      D: 500,
-      M: 1000
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
   }
-
+  
   let result = 0
   for(let i = 0; i < s.length; i++){
-    let num1 = map[s[i]]
-    let num2 = map[s[i+1]]
-    if(num2 > num1){
-      result = result + num2 - num1
+    if(i !== s.length-1 && map[s[i]] < map[s[i+1]]){
+      result = result + map[s[i+1]] - map[s[i]]
       i++
     }else{
-      result = result + num1
+      result = result + map[s[i]]  
     }
   }
   return result
