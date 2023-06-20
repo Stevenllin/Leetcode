87f09514c27d391a50a651a341fe5a43208c5a0f
Steven <stevenlin3263@gmail.com>
2023-06-20 09:10:54 +0800

    20. Valid Parentheses

--- String/Easy/20. Valid Parentheses.js
+++ String/Easy/20. Valid Parentheses.js
@@ -1,24 +1,32 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 1. 若遞迴當前值為 '(', '[', '{' ，將對應的 ')', ']', '}' push 至 @array
+ * 2. 若遞迴當前值並非 '(', '[', '{' ，將 @array pop 後比較當前遞迴值是否相同，若不同，即回傳 false
  * 
- * Time complexity O(n)
+ * 注意：最後，@array 的長度可能不為空，例如 @s 為 ['(']
+ * 
+ * 時間複雜度為 O(n)
 */
 var isValid = function(s) {
-  if((s.length % 2) !== 0) return false
-  
-  const object = {
+  if(s.length%2 !== 0) return false
+
+  const map = {
     '(': ')',
     '[': ']',
     '{': '}'
   }
-  const stack = []
+  const array = []
+
   for(let i = 0; i < s.length; i++){
-    if(object[s[i]]){
-      stack.push(object[s[i]])
+    if(s[i] === '(' || s[i] === '[' || s[i] === '{'){
+      array.push(map[s[i]])
     }else{
-      if(s[i] !== stack.pop()){
+      if(s[i] !== array.pop()){
         return false
       }
     }
   }
-  return stack.length === 0
+  return array.length === 0
 };
