1aa376f6a96e0221f8bca6e9a2043f616e1051fb
Steven <stevenlin3263@gmail.com>
2023-06-22 10:46:02 +0800

    88 Merge Sorted Array

--- Array/Easy/88 Merge Sorted Array.js
+++ Array/Easy/88 Merge Sorted Array.js
@@ -1,25 +1,21 @@
-/** 
- * Method 1
- * Strategy
+/** 練習兩次 */
+
+/** Method 1
+ * 策略：將兩個陣列 @nums1 @nums2 合併後，再透過 sort 方法進行排列
  * 
- * this solution is divided by three section
- * if the length of @nums2 is 0 (n === 0) return nums1
- * if the length of @nums1 is 0 (m === 0) let @nums1 store each element of @nums2
- * if (m !== 0) and (n !== 0), let @nums start storing each element of @nums2 from index @index and return sorted array @nums1
- * 
- * the time complexity is O(n)
+ * 時間複雜度為 O(nlogn)
 */
 
 var merge = function(nums1, m, nums2, n) {
   if (n === 0) return nums1
   if (m === 0) {
-      nums2.forEach((item, index) => nums1[index] = item)
-      return nums1
+    nums2.forEach((item, index) => nums1[index] = item)
+    return nums1
   }
   let index = 0
   for (let i = m; i < nums1.length; i++) {
-      nums1[i] = nums2[index]
-      index++
+    nums1[i] = nums2[index]
+    index++
   }
   return nums1.sort((a, b) => a - b)
 };
