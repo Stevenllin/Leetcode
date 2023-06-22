f08675f42a7bf59b243df9f8bb8c6e7cc2135e4a
Steven <stevenlin3263@gmail.com>
2023-06-22 21:49:26 +0800

    171. Excel Sheet Column Number

--- String/Easy/171. Excel Sheet Column Number.js
+++ String/Easy/171. Excel Sheet Column Number.js
@@ -1,18 +1,20 @@
-/** method 1 
- * 
- * use charCodeAt method to find number of each char and minus 64 to meet the corresponding column number
- * Notice that we don't need to convert this string to array
- * 
- * Time complexity O(n)
-*/
+/** 練習兩次 */
 
+/** Method 1 
+ * 策略：本題透過字串的 charCodeAt 的方法找到對應的數字，並減去 64 將字母歸一 @charNumber 。另外，字母與字母相差 26，因此，每一輪遞迴的當前 @charNumber 乘上對應 @times 加入最終回傳值 @number
+ * 
+ * 注意：遞迴是從字串的頭開始， @times 為 @columnTitle 的長度 - 1 - @i
+ * 
+ * 時間複雜度為 O(n)
+*/
 var titleToNumber = function(columnTitle) {
   let number = 0
 
   for(let i = 0; i < columnTitle.length; i++){
-    const target = columnTitle.charCodeAt(i) - 64
-    const times = columnTitle.length - i - 1
-    number = number + target * Math.pow(26, times)
+    const charNumber = columnTitle.charCodeAt(i) - 64
+    const times = columnTitle.length - 1 - i
+    number = number + charNumber * Math.pow(26, times)
   }
+
   return number
 };
