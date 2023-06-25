8531584c652d28e15e4b10192515d3c924bbd393
Steven <stevenlin3263@gmail.com>
2023-06-25 21:08:30 +0800

    168. Excel Sheet Column Title

--- String/Easy/168. Excel Sheet Column Title.js
+++ String/Easy/168. Excel Sheet Column Title.js
@@ -1,23 +1,20 @@
-/** method 1 
- * Strategy
- * 
- * remember add the new char to the front of @string
- * fromCharCode method is to covert the number to string
- * 
- * n should minus @target for the next loop
- * 
- * Time complexity O(n)
- */
+/** 練習兩次 */
 
+/** Method 1 
+ * 策略：組成 @string 的順序是從尾至頭。每一輪遞回除以 26 取餘數，將餘數換算成對應字母後加上上一輪的 @string 。最後，數字 @n 需減去餘數 @target 後再除以 26 取近似值。
+ * 
+ * 注意：若餘數為零，代表對應的字母是 Z
+ * 
+ * 時間複雜度為 O(n)
+ */
 var convertToTitle = function(n) {
   let string = ''
   while(n > 0){
     let target = n%26
-    if (target === 0) {
+    if(target !== 0) string = String.fromCharCode(target+64) + string
+    if(target === 0){
       string = 'Z' + string
       target = 26
-    }else{
-      string = String.fromCharCode(target + 64) + string
     }
     n = Math.floor((n-target)/26)
   }
