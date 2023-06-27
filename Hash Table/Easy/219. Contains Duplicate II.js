b69608f459b37c8ed3e29259e7ff96fa99e16038
Steven <stevenlin3263@gmail.com>
2023-06-27 12:44:49 +0800

    219. Contains Duplicate II

--- Hash Table/Easy/219. Contains Duplicate II.js
+++ Hash Table/Easy/219. Contains Duplicate II.js
@@ -1,19 +1,23 @@
-/** method 1 
- * 
- * create an object @object to store the number with current corresponding index
- * if index @i - the previous index of this value stored in @object that is smaller than or equal to @k then return true
- * 
- * Time complexity O(n)
- */
-var containsNearbyDuplicate = function(nums, k) {
-  if(nums.length <= 1) return false
+/** 練習兩次 */
 
-  let object = {}
+/** Method 1 
+ * 策略：@object 目的是存取對應數字上一次的 index @i ，若上一次 index 與此輪的 index 相差小於 @k ，則回傳 true。
+ * 
+ * 時間複雜度為 O(n)
+ */
+ var containsNearbyDuplicate = function(nums, k) {
+  const object = {}
+
   for(let i = 0; i < nums.length; i++){
-    if(i - object[nums[i]] <= k){
-      return true
-    }
-    object[nums[i]] = i
+    if(object[nums[i]] === null){
+      object[nums[i]] = i  
+    }else{
+      if(Math.abs(i-object[nums[i]]) <= k){
+        return true  
+      }else{
+        object[nums[i]] = i
+      }  
+    }  
   }
   return false
 };
