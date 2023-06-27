c941f4544cf67bc3c0ac5ce7d822e6d35d52b45b
Steven <stevenlin3263@gmail.com>
2023-06-27 09:07:26 +0800

    205. Isomorphic Strings

--- String/Easy/205. Isomorphic Strings.js
+++ String/Easy/205. Isomorphic Strings.js
@@ -1,42 +1,31 @@
-/** method 1
+/** 練習兩次 */
+
+/** Method 1
+ * 策略：將每個字母對應獨立的數字 @numberS @numberT 分別儲存於 @objectS @objectT 。每一次遞迴比較 @objectS @objectT 此次的 @numberS @numberT ，不相等即回傳 false
  * 
- * @objS @objT char as the key and the @numberS @numberT as the value
- * if char haven't been exist in @objS or @objT then add the @numberS or @numberT to 1 and store in @objS or @objT
- * And let @numberS or @numberT equal to @targetS or @targetT
- * 
- * if char is exist then set @targetS or @targetT to corresponding value of the key in @objS or @objT
- * Otherwise compare @targetS with @tagetT
- * 
- * Time complexity O(n)
+ * 時間複雜度為 O(n)
  */
 
-var isIsomorphic = function(s, t) {
+ var isIsomorphic = function(s, t) {
   if(s.length !== t.length) return false
-
-  let objS = {}
-  let objT = {}
+  
+  const objectS = {}
+  const objectT = {}
   let numberS = 0
   let numberT = 0
 
   for(let i = 0; i < s.length; i++){
-    let targetS = 0
-    let targetT = 0
-    if(!objS[s[i]]){
+    if(!objectS[s[i]]){
       numberS++
-      objS[s[i]] = numberS
-      targetS = numberS
-    }else{
-      targetS = objS[s[i]]
+      objectS[s[i]] = numberS
     }
-    if(!objT[t[i]]){
+    if(!objectT[t[i]]){
       numberT++
-      objT[t[i]] = numberT
-      targetT = numberT
-    }else{
-      targetT = objT[t[i]]
+      objectT[t[i]] = numberT
     }
-    if(targetS !== targetT) return false
+    if(objectS[s[i]] !== objectT[t[i]]) return false
   }
   return true
 };
+
 
