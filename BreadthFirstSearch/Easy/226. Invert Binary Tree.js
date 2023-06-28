e0599364c0835a8e4269f4b50a03ca559712b3c2
Steven <stevenlin3263@gmail.com>
2023-06-28 08:37:51 +0800

    226. Invert Binary Tree

--- BreadthFirstSearch/Easy/226. Invert Binary Tree.js
+++ BreadthFirstSearch/Easy/226. Invert Binary Tree.js
@@ -1,12 +1,16 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：觀察結果，發現每一層的左右分支交換這。
  * 
+ * 注意：因需先改變 @root 左分支，因此，記得 @temp 先暫存 @root 左分支
  */
- var invertTree = function(root) {
-  if (!root || (!root.left && !root.right)) return root
+var invertTree = function(root) {
+  if(!root) return root
 
   const temp = root.left
   root.left = invertTree(root.right)
   root.right = invertTree(temp)
-
+  
   return root
 };
