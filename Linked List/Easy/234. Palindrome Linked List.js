f4bda215003cc1570792706fa8e692be53c6373e
Steven <stevenlin3263@gmail.com>
2023-06-30 08:30:00 +0800

    234. Palindrome Linked List

--- Linked List/Easy/234. Palindrome Linked List.js
+++ Linked List/Easy/234. Palindrome Linked List.js
@@ -1,19 +1,20 @@
-/** method 1 
- * store all number in an array @array
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：將所有節點的值存於 @array ，儲存後查看 @array 內的元素排序是否為對稱
  * 
- * Time complexity O(n)
+ * 時間複雜度為 O(n)
  */
-
 var isPalindrome = function(head) {
-  if (head === null) return false
-
   const array = []
-  while (head.next) {
-    array.push(head.val)
-    head = head.next
+
+  while(head){
+    array.push(head.val)  
+    head = head.next  
   }
-  for(let i = 0; i < Math.floor(array.length); i++){
-    if(array[i] !== array[array.length - i - 1]) return false
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] !== array[array.length-1-i]) return false
   }
   return true
 };
