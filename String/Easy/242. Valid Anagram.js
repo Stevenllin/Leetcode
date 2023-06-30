42b1a77c03fe2c9c46b8b5f94306887c61d013e4
Steven <stevenlin3263@gmail.com>
2023-06-30 08:50:25 +0800

    242. Valid Anagram

--- String/Easy/242. Valid Anagram.js
+++ String/Easy/242. Valid Anagram.js
@@ -1,61 +1,13 @@
-/** method 1
+/** 練習兩次 */
+
+/** Method 1
+ * 策略：將 @s @t 先轉為陣列排序後，再轉為字串後進行比較
  * 
- * 
- * Time Complexity O(3n)
- * However, this method use 2 times sort(). So it is not really good method
+ * 時間複雜度為 O(nlogn)
  */
-var isAnagram = function(s, t) {
-  if(s.length !== t.length) return false
+ var isAnagram = function(s, t) {
+  const sortedS = [...s].sort().join('')
+  const sortedT = [...t].sort().join('')
 
-  const arrayS = [...s].sort()
-  const arrayT = [...t].sort()
-
-  for(let i = 0; i < arrayT.length; i++){
-    if(arrayT[i] !== arrayS[i]) return false
-  }
-  return true
-};
-
-/** method 2
- * 
- * Store all char and those appereace times in @objS
- * 
- * And use for loop to check if all char from @t is exist in @objS
- * 
- * Time Complexity O(2n)
-*/
-
-var isAnagram = function(s, t) {
-  if(s.length !== t.length) return false
-  
-  const objS = {}
-  for(let i = 0; i < s.length; i++){
-    if(objS[s[i]]){
-      objS[s[i]] = objS[s[i]] + 1
-    }else{
-      objS[s[i]] = 1
-    }
-  }
-  for(let i = 0; i < t.length; i++){
-    if(!objS[t[i]]){
-      return false
-    }else{
-      objS[t[i]] = objS[t[i]] - 1
-    }
-  }
-  return true
-};
-
-/** method 3
- * 
- * it's almost like method 1
- * the difference is covert to string
- */
-var isAnagram = function(s, t) {
-  if(s.length != t.length) return false;
-
-  var s = s.split("").sort().join("");
-  var t = t.split("").sort().join("");
-
-  return s == t;
+  return sortedS === sortedT
 };
