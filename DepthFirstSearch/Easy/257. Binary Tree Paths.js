fa8e923496b2674d951689ee9f1843a3f126eee9
Steven <stevenlin3263@gmail.com>
2023-07-01 10:48:16 +0800

    257. Binary Tree Paths

--- DepthFirstSearch/Easy/257. Binary Tree Paths.js
+++ DepthFirstSearch/Easy/257. Binary Tree Paths.js
@@ -1,5 +1,11 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * @string 是暫存的字串
+ * 遍歷樹的每個節點，若節點左右點存在 @string + 當前 @root 的 @val 與 "->"，並傳入下一個節點；若節點不存在時，要將暫存的字串 @string + 當前 @root 的 @val 並存入 @array
  * 
+ * 時間複雜度為 O(n)
  */
 var binaryTreePaths = function(root) {
   if(!root) return []
