5d6171f42f49473e820a4cc5f31da6c332f196fd
Steven <stevenlin3263@gmail.com>
2023-08-08 17:43:16 +0800

    206. Reverse Linked List

--- Linked List/Easy/206. Reverse Linked List.js
+++ Linked List/Easy/206. Reverse Linked List.js
@@ -1,4 +1,8 @@
-/** method 1
+/** Method 1
+ * 策略：
+ * 將每一輪目前節點  @temp 的下一節點連接至前一個節點 @previous
+ * 
+ * 注意：每一輪要記得重新設定前一個節點 @previous 等於每輪目前節點 @temp
  * 
  * Time complexity O(n)
 */
@@ -9,12 +13,17 @@ var reverseList = function(head) {
 
   let previous = head
   let current = head.next
+  /** 將第一節點 node 之後的所有連結設為 null  */
   previous.next = null
 
   while(current){
+    /** 目前的節點 node */
     let temp = current
+    /** 移致下一節點 */
     current = current.next
+    /** 將目前的節點的下一點連結至上一點 */
     temp.next = previous
+    /** 設定上一點等於目前的 node */
     previous = temp
   }
 
