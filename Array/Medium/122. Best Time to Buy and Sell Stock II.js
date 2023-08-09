5615877bbb318b0de5873d5a8814f98961ca4645
Steven <stevenlin3263@gmail.com>
2023-08-09 17:58:41 +0800

    122. Best Time to Buy and Sell Stock II Greedy solution

--- Array/Medium/122. Best Time to Buy and Sell Stock II.js
+++ Array/Medium/122. Best Time to Buy and Sell Stock II.js
@@ -1,14 +1,16 @@
-/** method 1 
+/** Method 1 貪心
+ * 策略：
+ * 碰到前一天價格小於當前價格即計算入最終收益 @benefit
  * 
+ * 時間複雜度為 O(n)
  */
 var maxProfit = function(prices) {
   let benefit = 0
 
-  for (let i = 1; i < prices.length; i++) {
+  for (let i = 0; i < prices.length; i++) {
     if (prices[i] - prices[i-1] > 0) {
       benefit = benefit + prices[i] - prices[i-1]
     }
   }
-
   return benefit
 };
