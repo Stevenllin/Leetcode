f2f1d4058aa3fe093ab28c1c70ac5cc792a55c73
Steven <stevenlin3263@gmail.com>
2023-08-09 09:25:31 +0800

    121. Best Time to Buy and Sell Stock：動態規劃

--- Array/Easy/121. Best Time to Buy and Sell Stock.js
+++ Array/Easy/121. Best Time to Buy and Sell Stock.js
@@ -19,3 +19,26 @@ var maxProfit = function(prices) {
 
   return maxProfit
 };
+
+/** Method 2 
+ * 策略：動態規劃
+ * dp[i][0]：代表第 i 天持有股票的最大收益（可在第 i 天以前或是當天買入）
+ * dp[i][1]：代表第 i 天不持有股票的最大收益（可在第 i 天以前或是當天賣出）
+ * 
+ * 每一輪比較 dp[i] 的第一個元素、第二個元素
+ * 第一個元素：過去最大買入收益與當天買入收益
+ * 第二個元素：過去最大賣出收益與當天賣出收益
+ */
+var maxProfit = function(prices) {
+  const dp = new Array(prices.length).fill([0, 0])
+  /** 陣列 dp 初始化 */
+  dp[0] = [-prices[0], 0]
+  
+  for (let i = 1; prices.length; i++) {
+    dp[i] = [
+      Math.max(dp[i-1][0], -prices[i]),
+      Math.max(dp[i-1][1], prices[i] + dp[i-1][0])
+    ]
+  }
+  return dp[prices.length-1][1]
+};
