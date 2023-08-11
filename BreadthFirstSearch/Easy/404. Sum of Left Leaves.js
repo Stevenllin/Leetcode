a3e6a79d4aa1a2c221f2764f211c9118ffa85d2f
Steven <stevenlin3263@gmail.com>
2023-08-11 17:15:32 +0800

    404. Sum of Left Leaves

--- BreadthFirstSearch/Easy/404. Sum of Left Leaves.js
+++ BreadthFirstSearch/Easy/404. Sum of Left Leaves.js
@@ -1,5 +1,12 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 遍歷 Tree，並透過變數 @isLeaf 判斷節點來源是否為左邊分支
+ * 若是左分支且無子分支，則將此點的值相加
  * 
+ * 
+ * 時間複雜度為 O(n)
 */
 var sumOfLeftLeaves = function(root) {
 
@@ -12,6 +19,7 @@ var sumOfLeftLeaves = function(root) {
     }
     result = addLeaf(root.left, result, true)
     result = addLeaf(root.right, result, false)
+
     return result
   }
 };
