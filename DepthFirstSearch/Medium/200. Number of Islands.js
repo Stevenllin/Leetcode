453c842db1d57151a7b6580e04721bb91cf664ba
Steven <stevenlin3263@gmail.com>
2023-08-21 09:10:28 +0800

    200. Number of Islands

--- DepthFirstSearch/Medium/200. Number of Islands.js
+++ DepthFirstSearch/Medium/200. Number of Islands.js
@@ -1,30 +1,36 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 1.透過 DFS 函數尋找 island，僅檢查當前點為 1。此外，若碰到當前點為 1，則覆寫為 'A'，代表已走過該點。反之，return 函數
  * 
+ * 時間複雜度為：O(n^2)
  */
-var numIslands = function(grid) {
+ var numIslands = function(grid) {
   const rows = grid.length
   const columns = grid[0].length
+
   let result = 0
 
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-      if (grid[i][j] === '1') {
+  for(let row = 0; row < rows; row++){
+    for(let column = 0; column < columns; column++){
+      if(grid[row][column] === '1'){
         result++
-        findElement(i, j)
+        helper(row, column)  
       }
-    }
+    }  
   }
+
   return result
 
-  function findElement (i, j) {
-    if (i < 0 || j < 0 || i >= rows || j >= columns) return
-    if (grid[i][j] === 'A' || grid[i][j] === '0') return
-    if (grid[i][j] === '1') {
+  function helper(i, j){
+    if(i < 0 || i >= rows || j < 0 || j >= columns || grid[i][j] === 'A' || grid[i][j] === '0') return
+    if(grid[i][j] === '1'){
       grid[i][j] = 'A'
-      findElement(i+1, j)
-      findElement(i-1, j)
-      findElement(i, j+1)
-      findElement(i, j-1)
+      helper(i-1, j)
+      helper(i+1, j)
+      helper(i, j-1)
+      helper(i, j+1)
     }
   }
 };
