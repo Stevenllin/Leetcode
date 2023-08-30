0d5590edb31c6c998d7e7f3ebe8dffd89b9ae0a9
Steven <stevenlin3263@gmail.com>
2023-08-30 12:26:27 +0800

    54. Spiral Matrix

--- Array/Medium/54. Spiral Matrix.js
+++ Array/Medium/54. Spiral Matrix.js
@@ -1,37 +1,44 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 1. 設定 上下左右 index @top @bottom @left @right
+ * 2. 每一輪透過順時針將數字傳遞至 @array ，要將 @top @left 加 1、@bottom @right 減一，直到 @top 大於 @bottom 、 @left 大於 @right
  * 
+ * 備註：在下方、左方，要加入 if 判斷條件，因為在上方、右方時，@top 加、 1 @right 減一
+ * 
+ * 時間複雜度為：
  */
 var spiralOrder = function(matrix) {
+  let top = 0
+  let bottom = matrix.length - 1
   let left = 0
   let right = matrix[0].length - 1
-  let bottom = matrix.length - 1
-  let top = 0
+  const array = []
 
-  const result = []
-
-  while(left <= right && top <= bottom){
-    /** top */
+  while(top <= bottom && left <= right){
+    /** left to right */
     for(let i = left; i <= right; i++){
-      result.push(matrix[top][i])
+      array.push(matrix[top][i])  
     }
-    /** right */
-    for(let i = top+1; i <= bottom; i++){
-      result.push(matrix[i][right])
+    top++
+    /** top to bottom */
+    for(let i = top; i <= bottom; i++){
+      array.push(matrix[i][right])  
     }
-    /** bottom */
-    for(let i = right-1; i >= left; i--){
-      if(top === bottom) break
-      result.push(matrix[bottom][i])
+    right--
+    /** right to left */
+    for(let i = right; i >= left; i--){
+      if(top > bottom) break
+      array.push(matrix[bottom][i])
     }
-    /** left */
-    for(let i = bottom-1; i > top; i--){
-      if(left === right) break
-      result.push(matrix[i][left])
+    bottom--
+    /** bottom to top */
+    for(let i = bottom; i >= top; i--){
+      if(left > right) break
+      array.push(matrix[i][left])   
     }
     left++
-    right--
-    bottom--
-    top++
   }
-  return result
+  return array
 };
