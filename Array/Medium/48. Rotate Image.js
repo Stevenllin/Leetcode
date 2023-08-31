9a821f955df5fab5cc24bfae14cf8a3158c6087a
Steven <stevenlin3263@gmail.com>
2023-08-31 08:39:57 +0800

    48. Rotate Image

--- Array/Medium/48. Rotate Image.js
+++ Array/Medium/48. Rotate Image.js
@@ -1,22 +1,26 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 1. 水平交換
+ * 2. 左對角線交換
  * 
+ * 時間複雜度為：O(mn)
  */
 var rotate = function(matrix) {
-  /**  */
   for(let i = 0; i < Math.floor(matrix.length/2); i++){
     const temp = [...matrix[i]]
     matrix[i] = [...matrix[matrix.length-1-i]]
     matrix[matrix.length-1-i] = temp
   }
-  /** Diagonal reversed */
+
   for(let i = 0; i < matrix.length; i++){
-    for(let j = 0; j < matrix.length; j++){
-      if(j > i){
+    for(let j = 0; j < matrix[0].length; j++){
+      if(i > j){
         const temp = matrix[i][j]
         matrix[i][j] = matrix[j][i]
         matrix[j][i] = temp
       }
     }
   }
-  return matrix
 };
