51a9147400a8541a9d44ad41795a08f45fd0cf9d
Steven <stevenlin3263@gmail.com>
2023-09-01 09:32:00 +0800

    79. Word Search

--- Array/Medium/79. Word Search.js
+++ Array/Medium/79. Word Search.js
@@ -1,28 +1,39 @@
-/** method 1 
+/** 練習兩次 */
+
+/** Method 1 
+ * 策略：
+ * 1. 使用 Nested For Loop 檢查當前值是否與 @word 第一個字母相同，若相同則進入 @helper 。此外，若 @helper 回傳為 true 則直接回傳 true
+ * 2. 每一輪 @helper 檢查當前 @index 與 @word 的長度是否相同。如果要檢查得字母與 @board 當前值相同且 不@board 當前值不為 # 則繼續執行 @helper (代入上下左右)
  * 
+ * 備註：
+ * 1. 使用 # 字，標示已走過的路徑，並使用暫存的 @temp 還原原來的字母
+ * 
+ * 時間複雜度為：
  */
- var exist = function(board, word) {
-  if (word === "") return true
+
+var exist = function(board, word) {
   for(let i = 0; i < board.length; i++){
     for(let j = 0; j < board[0].length; j++){
       if(board[i][j] === word[0]){
-        if(search(0, i, j)) return true
+        if(helper(i, j, 0)) return true
       }
     }
   }
+
   return false
 
-  function search(index, row, column){
+  function helper(i, j, index){
     if(index === word.length) return true
-    if(!board[row] || !board[row][column]) return false
-    if(board[row][column] !== "#" && board[row][column] === word[index]){
-      const target = board[row][column]
-      board[row][column] = "#"
-      if(search(index+1, row-1, column)) return true
-      if(search(index+1, row+1, column)) return true
-      if(search(index+1, row, column-1)) return true
-      if(search(index+1, row, column+1)) return true
-      board[row][column] = target
+    if(!board[i] || !board[i][j]) return false
+
+    if(board[i][j] !== '#' && word[index] === board[i][j]){
+      const temp = board[i][j]
+      board[i][j] = '#'  
+      if(helper(i+1, j, index+1)) return true
+      if(helper(i-1, j, index+1)) return true
+      if(helper(i, j+1, index+1)) return true
+      if(helper(i, j-1, index+1)) return true
+      board[i][j] = temp
     }
     return false
   }
