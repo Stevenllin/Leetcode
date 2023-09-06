e857093e2991bad39780ef65c8e56fb6682998ca
Steven <stevenlin3263@gmail.com>
2023-09-06 08:54:54 +0800

    242. Valid Anagram

--- String/Easy/242. Valid Anagram.js
+++ String/Easy/242. Valid Anagram.js
@@ -12,3 +12,28 @@ var isAnagram = function(s, t) {
 
   return sortedS === sortedT
 };
+
+/** Method 2 
+ * 策略：
+ * 1. 透過 @freq 儲存每個字母狀態，以字母為 key 
+ * 2. 第一個 for loop 每一輪 @s 以 +1 代表出現， @t 以 -1 
+ * 3. 第二個 for loop 檢查 @freq 是否有 value 不為 0
+ * 
+ * 時間複雜度為 O(n)
+ */
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false;
+
+  var freq = new Array(26).fill(0);
+
+  for (var i = 0; i < s.length; i++) {
+    freq[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    freq[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+  }
+
+  for (var i = 0; i < freq.length; i++) {
+    if (freq[i] !== 0) return false;
+  }
+
+  return true;
+};
