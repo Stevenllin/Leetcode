b04295b8a901ff94152d652ed03b3b66d457e6c5
Steven <stevenlin3263@gmail.com>
2023-09-06 08:41:05 +0800

    242. Valid Anagram

--- String/Easy/242. Valid Anagram.js
+++ String/Easy/242. Valid Anagram.js
@@ -5,7 +5,8 @@
  * 
  * 時間複雜度為 O(nlogn)
  */
- var isAnagram = function(s, t) {
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false
   const sortedS = [...s].sort().join('')
   const sortedT = [...t].sort().join('')
 
